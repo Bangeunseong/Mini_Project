0cbff5ca52a9dfb865fe585c2ea18503779e38fc
Bangeunseong <bangeunseong@naver.com>
2025-04-08 15:53:53 +0900

    Edit : File Directory, Card Controller

--- Assets/Scripts/Controller Scripts/BoardController.cs
+++ Assets/Scripts/Controller Scripts/BoardController.cs
@@ -3,25 +3,29 @@ using UnityEngine;
 
 public class BoardController : MonoBehaviour
 {
+    private int _category;
     public GameObject CardPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (!PlayerPrefs.HasKey("Category")) { Debug.LogError("Cannot find key of category!"); _category = 0; }
+        else _category = PlayerPrefs.GetInt("Category");
+
         SpawnCards();
     }
 
     void SpawnCards()
     {
         // Position
-        int cardCountRow = 4;
-        int cardCount = cardCountRow * cardCountRow;
+        int cardCountRow = 4, cardCountColumn = 5;
+        int cardCount = cardCountRow * cardCountColumn;
         float xOffset = (-(cardCountRow * 1.3f + (cardCountRow - 1) * 0.1f) + 1.3f) / 2f;
         float yOffset = -3f;
 
         // Image Randomization
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+        int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+        arr = arr.OrderBy(x => Random.Range(0f, 19f)).ToArray();
 
         Vector2 position = new Vector2(xOffset, yOffset);
         for (int i = 0; i < cardCount; i++)
@@ -33,7 +37,7 @@ public class BoardController : MonoBehaviour
             }
             GameObject card = Instantiate(CardPrefab, transform);
             card.transform.position = position;
-            card.GetComponent<CardController>().Set(arr[i]);
+            card.GetComponent<CardController>().Set(_category, arr[i]);
             position.x += 1.4f;
         }
 
