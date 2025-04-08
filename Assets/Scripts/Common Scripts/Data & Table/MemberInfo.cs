e22361cd76be12b3d60fc6f51a0d3be767b94f48
Bangeunseong <bangeunseong@naver.com>
2025-04-08 12:15:13 +0900

    Edit : Image Changed

---
+++ Assets/Scripts/Common Scripts/Data & Table/MemberInfo.cs
@@ -0,0 +1,26 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable] public class Pair
+{
+    public int Key;
+    public List<ImageInfo> Values;
+}
+
+[Serializable] public class ImageInfo
+{
+    public int ParentId;
+    public string Description;
+    public Sprite Image;
+}
+
+[Serializable] public class MemberInfo
+{
+    public int Id;
+    public string Name;
+    public List<Sprite> Selfies;
+    public List<Pair> PairOfImages;
+}
