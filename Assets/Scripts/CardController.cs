0300f44ca674244ac34935b17e8677f6601e7fa8
Seungmin-Yang <mse9719@gmail.com>
2025-04-08 09:36:03 +0900

    Feat: unity ui 추가
    - unity ui 추가

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -3,46 +3,46 @@ using UnityEngine;
 
 public class CardController : MonoBehaviour
 {
-    private AudioSource audioSource;
-    private Animator animator;
+    private AudioSource _audioSource;
+    private Animator _animator;
     
-    public int index { get; private set; }
-    public SpriteRenderer image;
-    public GameObject front;
-    public GameObject back;
-    public AudioClip clip;
+    public int Index { get; private set; }
+    public SpriteRenderer Image;
+    public GameObject Front;
+    public GameObject Back;
+    public AudioClip Clip;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
+        _animator = GetComponent<Animator>();
     }
 
     public void Set(int index) { 
-        this.index = index;
-        image.sprite = Resources.Load<Sprite>($"rtan{index}");
+        this.Index = index;
+        Image.sprite = Resources.Load<Sprite>($"rtan{index}");
     }
 
     public void Open()
     {
-        if (!GameManager.instance.isGameActive) { return; }
+        if (!GameManager.Instance.IsGameActive) { return; }
 
-        audioSource.PlayOneShot(clip);
-        animator.SetBool("IsOpen", true);
-        front.SetActive(true);
-        back.SetActive(false);
-        if (GameManager.instance.firstCard == null) GameManager.instance.firstCard = this;
+        _audioSource.PlayOneShot(Clip);
+        _animator.SetBool("IsOpen", true);
+        Front.SetActive(true);
+        Back.SetActive(false);
+        if (GameManager.Instance.FirstCard == null) GameManager.Instance.FirstCard = this;
         else
         {
-            GameManager.instance.secondCard = this;
-            GameManager.instance.MatchCards();
+            GameManager.Instance.SecondCard = this;
+            GameManager.Instance.MatchCards();
         }
     }
 
     IEnumerator DestroyCardRoutine()
     {
-        yield return new WaitForSeconds(1f / GameManager.instance.wave);
+        yield return new WaitForSeconds(1f / GameManager.Instance.Wave);
         Destroy(gameObject);
     }
 
@@ -53,10 +53,10 @@ public class CardController : MonoBehaviour
 
     IEnumerator CloseCardRoutine()
     {
-        yield return new WaitForSeconds(1f / GameManager.instance.wave);
-        animator.SetBool("IsOpen", false);
-        front.SetActive(false);
-        back.SetActive(true);
+        yield return new WaitForSeconds(1f / GameManager.Instance.Wave);
+        _animator.SetBool("IsOpen", false);
+        Front.SetActive(false);
+        Back.SetActive(true);
     }
 
     public void CloseCard()
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,14 +7,14 @@ public class GameManager : MonoBehaviour
     private static GameManager m_instance;
     private AudioSource audioSource;
 
-    public CardController firstCard, secondCard;
-    public Text timeText;
-    public AudioClip clip;
-    public GameObject endText;
-    public bool isGameActive {  get; private set; }
-    public int wave { get; private set; }
-    public int cardCount;
-    public static GameManager instance
+    public CardController FirstCard, SecondCard;
+    public Text TimeText;
+    public AudioClip Clip;
+    public GameObject EndText;
+    public bool IsGameActive {  get; private set; }
+    public int Wave { get; private set; }
+    public int CardCount;
+    public static GameManager Instance
     {
         get { 
             if(m_instance == null) 
@@ -26,47 +26,47 @@ public class GameManager : MonoBehaviour
     // Awake is called once before Start Method
     void Awake()
     {
-        if (!PlayerPrefs.HasKey("Wave")) wave = 1;
-        else wave = PlayerPrefs.GetInt("Wave");
+        if (!PlayerPrefs.HasKey("Wave")) Wave = 1;
+        else Wave = PlayerPrefs.GetInt("Wave");
 
-        if (instance != this) Destroy(gameObject);
+        if (Instance != this) Destroy(gameObject);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startTime = 0;
-        isGameActive = true;
+        IsGameActive = true;
         audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!isGameActive) { return; }
-        if(cardCount <= 0) { isGameActive = false; endText.SetActive(true); PlayerPrefs.SetInt("Wave", wave++); return; }
-        if(startTime >= 30) { isGameActive = false; endText.SetActive(true); PlayerPrefs.SetInt("Wave", 1); return; }
+        if(!IsGameActive) { return; }
+        if(CardCount <= 0) { IsGameActive = false; EndText.SetActive(true); PlayerPrefs.SetInt("Wave", Wave++); return; }
+        if(startTime >= 30) { IsGameActive = false; EndText.SetActive(true); PlayerPrefs.SetInt("Wave", 1); return; }
         startTime += Time.deltaTime;
         UpdateTime();
     }
 
     void UpdateTime()
     {
-        timeText.text = startTime.ToString("N2");
+        TimeText.text = startTime.ToString("N2");
     }
 
     public void MatchCards()
     {
-        if(firstCard.index == secondCard.index) 
+        if(FirstCard.Index == SecondCard.Index) 
         {
-            audioSource.PlayOneShot(clip);
-            firstCard.DestroyCard(); secondCard.DestroyCard();
-            cardCount -= 2;
+            audioSource.PlayOneShot(Clip);
+            FirstCard.DestroyCard(); SecondCard.DestroyCard();
+            CardCount -= 2;
         }
         else
         {
-            firstCard.CloseCard(); secondCard.CloseCard();    
+            FirstCard.CloseCard(); SecondCard.CloseCard();    
         }
-        firstCard = secondCard = null;
+        FirstCard = SecondCard = null;
     }
 }
