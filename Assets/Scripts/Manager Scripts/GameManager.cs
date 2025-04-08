57848217bbdca2bb477efba574a51e9aa4a64f2e
Bangeunseong <bangeunseong@naver.com>
2025-04-08 21:16:26 +0900

    Edit : Added Description of Functions

--- Assets/Scripts/Manager Scripts/GameManager.cs
+++ Assets/Scripts/Manager Scripts/GameManager.cs
@@ -35,18 +35,22 @@ public class GameManager : MonoBehaviour
     // Awake is called once before Start Method
     void Awake()
     {
+        // Initialize Wave Attribute
         if (!PlayerPrefs.HasKey("Wave")) Wave = 1;
         else Wave = PlayerPrefs.GetInt("Wave");
 
+        // Initialize Category Attribute
         if (!PlayerPrefs.HasKey("Category")) Category = Category.Food;
         else Category = (Category)PlayerPrefs.GetInt("Category");
 
+        // If this gameobject is not belong to previously assigned GameManager, destroy it to prevent double init.
         if (Instance != this) Destroy(gameObject);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Init. Attributes
         startTime = 0;
         IsGameActive = true;
         audioSource = GetComponent<AudioSource>();
@@ -57,7 +61,10 @@ public class GameManager : MonoBehaviour
     {
         if(!IsGameActive) { return; }
 
+        // If All cards destroyed, game ends.
         if(CardCount <= 0) { IsGameActive = false; EndText.GetComponent<Text>().text = "¼º°ø!"; EndText.SetActive(true); PlayerPrefs.SetInt("Wave", Wave++); return; }
+        
+        // If Time passes over 30 seconds, game ends. -> Need modification ( Change static value(which is 30) to dynamic value )
         if(startTime >= 30) 
         { 
             IsGameActive = false;
@@ -66,7 +73,11 @@ public class GameManager : MonoBehaviour
             PlayerPrefs.SetInt("Wave", 1); 
             return; 
         }
+
+        // Update Time
         startTime += Time.deltaTime;
+
+        // Update Time UI
         UpdateTime();
     }
 
@@ -77,6 +88,7 @@ public class GameManager : MonoBehaviour
 
     public void MatchCards()
     {
+        // If both cards are all face images or category images, close cards and reset memory.
         if ((FirstCard.Index >= 10 && SecondCard.Index >= 10) || (FirstCard.Index < 10 && SecondCard.Index < 10)) 
         { 
             FirstCard.CloseCard(); SecondCard.CloseCard(); 
@@ -84,6 +96,8 @@ public class GameManager : MonoBehaviour
             return;
         }
 
+        // When FirstCard is not face images,
+        // Compare Second Card (Id -> Member Id) and First Card (Parent Id -> Connected Member Id)
         if(FirstCard.Id < 0)
         {
             if(SecondCard.Id == FirstCard.ParentId)
@@ -94,16 +108,22 @@ public class GameManager : MonoBehaviour
             }
             else FirstCard.CloseCard(); SecondCard.CloseCard();
         }
+        // When SecondCard is not face images,
+        // Compare First Card (Parent Id -> Paired Member Id) and Second Card (Id -> Member Id)
         else
         {
             if (FirstCard.Id == SecondCard.ParentId)
             {
+                // Play Audio and Destroy Cards
                 audioSource.PlayOneShot(Clip);
                 FirstCard.DestroyCard(); SecondCard.DestroyCard();
                 CardCount -= 2;
             }
+            // Close Cards
             else FirstCard.CloseCard(); SecondCard.CloseCard();
         }
+
+        // Reset Memory
         FirstCard = SecondCard = null;
     }
 }
